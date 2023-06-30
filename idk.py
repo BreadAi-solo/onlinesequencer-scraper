03fccb1007496d652d525a59da983c5755d15cea
breadbrowser <64813014+breadbrowser@users.noreply.github.com>
2023-06-30 14:18:31 -0400

    Add files via upload

--- idk.py
+++ idk.py
@@ -1,12 +1,39 @@
-# Import requests, BeautifulSoup, tqdm, and itertools libraries
+# Import requests, BeautifulSoup, tqdm, itertools, time, random, and selenium libraries
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 from itertools import count
 import time
 import random
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import os
+
+
+download_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'midi')
+
+
+
+# Set up the Chrome options
+chrome_options = Options()
+chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-plugins-discovery')
+chrome_options.add_argument('--disable-plugins')
+chrome_options.add_argument('--headless') # Run Chrome in headless mode
+
+chrome_options.add_experimental_option('prefs', {
+    "download.default_directory": download_dir,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True
+})
+# Set up the web driver (replace with the path to your web driver)
+driver = webdriver.Edge(options=chrome_options)
+
 # Define the base url and the initial start parameter
-base_url = "https://onlinesequencer.net/sequences?date=4&sort=2&start="
+base_url = "https://onlinesequencer.net/sequences?search=&sort=2&date=4&search="
 start = 1
 
 # Loop until there is no next page with a progress bar
@@ -27,27 +54,17 @@ for _ in tqdm(count(), desc="Scraping pages", unit="page"):
         href = link["href"]
         # Get the numbers from the href
         numbers = href[1:]
-        # Construct the midi url with the numbers
-        midi_url = f"https://onlinesequencer.net/app/midi.php?id={numbers}"
-        # Try to get the midi file from the url
-        midi_response = requests.get(midi_url)
-        # Check if the status code is 200 (OK)
-        if midi_response.status_code == 200:
-            # Try to save the midi file to a local file with the same name as the url
-            try:
-                with open(midi_url.split("=")[-1]+str(random.randrange(1,1000000000000))+".mid", "wb") as f:
-                    f.write(midi_response.content)
-                # Print a message that the file was saved
-                print(f"Saved {midi_url}")
-                
-            except PermissionError:
-                # Print a message that the file could not be saved and skip it
-                print(f"Could not save {midi_url}")
-                
-        else:
-            # Print a message that there was no file
-            print(f"No file at {midi_url}")
+        # Construct the sequence url with the numbers
+        sequence_url = f"https://onlinesequencer.net/{numbers}"
+        # Open the sequence page with Selenium
+        driver.get(sequence_url)
+        # Execute the exportMidi function in the console
+        driver.execute_script('return exportMidi();')
+        # Wait for a short time to allow the file to download (you may need to adjust this value)
+        time.sleep(0.2)
             
     # Increment the start parameter by 90 to go to the next page
     start += 90
 
+# Close the web driver when finished
+driver.close()
